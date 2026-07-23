93c1668b94d9d18129b3739656311906d0a6ed76
luyouqi9527 <hjl830112@outlook.com>
2026-07-23 18:00:00 +0800

    Update timer_provider.dart

--- lib/providers/timer_provider.dart
+++ lib/providers/timer_provider.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
